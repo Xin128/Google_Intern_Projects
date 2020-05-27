c1b07b57dda8bc961f28c1bac0ef361582ef4378
XinHao <xh23@rice.edu>
2020-05-27 15:26:24 -0500

    clear all unused files

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -23,10 +23,13 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Xin!");
-  }
+    
+     ArrayList<String> lst = new ArrayList<String>();
+     
+     
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;");
+        response.getWriter().println("Hello Xin!");
+    }
 }
