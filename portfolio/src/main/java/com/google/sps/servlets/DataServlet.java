ce351a27f88f0710d3de4e92f5386b2ad19dbb26
XinHao <xh23@rice.edu>
2020-05-29 14:14:47 -0500

    pom.xml; appengine-web.xml changed to include Project ID; script and index changed with different name of 'Hello'

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -23,6 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+     
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
