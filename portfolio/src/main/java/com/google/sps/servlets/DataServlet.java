86e9a10f637dffbefcd688658ba2a25b1fca58d5
XinHao <xh23@rice.edu>
2020-06-09 14:00:04 +0000

    delete unused import statements

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -30,7 +30,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 
 /**
